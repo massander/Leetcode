655cfc9540cabd43e9b7df3b562e087994161810
2thousandmax <2thousandmax@gmail.com>
2021-06-12 08:47:11 +0300

    Add 0448

---
+++ 0448-FindAllNumbersDisappearedInAnArray/solution.go
@@ -0,0 +1,21 @@
+package main
+
+func findDisappearedNumbers(nums []int) []int {
+    marked := make(map[int]bool)
+    disNums := make([]int, 0)
+    
+    for _, val := range nums{
+        if marked[val]{
+            continue
+        }
+        marked[val] = true
+    }
+    
+    for i:=1; i<len(nums)+1; i++ {
+        if !marked[i]{
+            disNums = append(disNums, i)
+        }
+    }
+    
+    return disNums
+}

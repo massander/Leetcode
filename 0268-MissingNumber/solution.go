cf37d4fc84b4a20795328fa870dd68e42440afae
2thousandmax <2thousandmax@gmail.com>
2021-06-12 08:30:40 +0300

    Add 0268

---
+++ 0268-MissingNumber/solution.go
@@ -0,0 +1,16 @@
+package main
+
+func missingNumber(nums []int) int {
+    marked := make(map[int]bool)
+    for _, val := range nums{
+        marked[val] = true
+    }
+    
+    for i:=0; i<len(nums)+1; i++ {
+        if !marked[i]{
+            return i
+        }
+    }
+    
+    return 0
+}

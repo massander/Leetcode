740c9e7dfb0512f31a3c2ed317b19d52d0b5d1d0
2thousandmax <2thousandmax@gmail.com>
2021-06-12 08:58:27 +0300

    Add 0136

---
+++ 0136-SingleNumber/solution.go
@@ -0,0 +1,17 @@
+package main
+
+func singleNumber(nums []int) int {
+    marked := make(map[int]int)
+    
+    for _, val:= range nums{
+        marked[val]++
+    }
+    
+    for i, val:= range marked{
+        if val == 1{
+            return i
+        }
+    }
+
+    return 0
+}

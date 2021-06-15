e39a29ab6681491705a0bf55f82f537170118297
2thousandmax <2thousandmax@gmail.com>
2021-06-15 09:17:13 +0300

    Add 0070

---
+++ 0070-ClimbingStairs/solution.go
@@ -0,0 +1,16 @@
+package main
+
+func climbStairs(n int) int {
+	if n < 4 {
+		return n
+	}
+
+	pre1 := 1
+	pre2 := 2
+
+	for i := 3; i <= n; i++ {
+		pre1, pre2 = pre2, pre1+pre2
+	}
+
+	return pre2
+}

6148c4729f1be6cba78a537f8c30160709484974
2thousandmax <2thousandmax@gmail.com>
2021-11-17 12:44:49 +0300

    Move structures to separate files, so they are reusable

--- 0002_AddTwoNumbers.go
+++ 0002_AddTwoNumbers.go
@@ -8,11 +8,6 @@ package main
  * }
  */
 
-type ListNode struct {
-	Val  int
-	Next *ListNode
-}
-
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	result := new(ListNode)
 	current := result

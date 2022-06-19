31a0e58bed3c220608c525d97467a49e5b4b0933
Max <2thousandmax@gmail.com>
2022-06-19 13:25:48 +0300

    Time: 6 ms (87.59%), Space: 4.6 MB (75.17%) - LeetHub

---
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.go
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func removeElements(head *ListNode, val int) *ListNode {
+    if head == nil{ return nil }
+    
+    start := new(ListNode); start.Next = head
+    current := start
+    
+    for current.Next != nil {
+        if current.Next.Val == val{
+            current.Next = current.Next.Next
+            continue
+        }
+        current = current.Next
+    }
+    return start.Next
+}

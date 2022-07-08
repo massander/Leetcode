32b8b50ed722e22258ce9ebf1cb57792c4d161fa
Max <2thousandmax@gmail.com>
2022-07-08 14:51:14 +0300

    Time: 21 ms (57.22%), Space: 7.4 MB (55.08%) - LeetHub

---
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.go
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func mergeTrees(root1 *TreeNode, root2 *TreeNode) *TreeNode {
+    if root1 == nil && root2 == nil{
+        return nil
+    }
+    
+    if root1 == nil{
+        return root2
+    }
+    
+    if root2 == nil{
+        return root1
+    }
+    
+    newRoot := &TreeNode{Val: root1.Val+root2.Val}
+    
+    newRoot.Left = mergeTrees(root1.Left, root2.Left)
+    newRoot.Right = mergeTrees(root1.Right, root2.Right)
+    
+    return newRoot
+}
